8d8f7ec0b9d22e2cc592e99a81edc768748c02f8
GiuliaGlave <giulia.glave@gmail.com>
2023-02-21 17:01:20 +0100

    footer bottom data

--- js/script.js
+++ js/script.js
@@ -36,6 +36,82 @@ const app = Vue.createApp({
           button: "ORDER NOW",
         },
       ],
+
+      footerTop: [
+        //logo + icons
+        {
+          logo: "img-logo",
+          text: "War should never be entered",
+          socialAccounts: [
+            {
+              facebookAccount: "fb-logo",
+              googleAccount: "g+-logo",
+              instagramAccount: "ig-logo",
+              pinterestAccount: "pts-logo",
+            },
+          ],
+        },
+        //emergency links
+        {
+          emergencyLinks: [
+            {
+              linkTitle: "Emergency Link.",
+              linkShop: "Organic Food Shop",
+              linkTerms: "Our Term & Condition",
+              linkFaq: "Gogrin Faq Section",
+              linkTeam: "Our Team Member",
+              linkNews: "Our Latest News Feed",
+            },
+          ],
+        },
+        //latest blog
+        {
+          blog: [
+            {
+              blogTitle: "Our Latest Blog.",
+              postPreview: [
+                {
+                  postImg: "img-blog-1",
+                  postText: "text-blog-1",
+                  postDate: "date-blog-1",
+                },
+                {
+                  postImg: "img-blog-2",
+                  postText: "text-blog-2",
+                  postDate: "date-blog-2",
+                },
+              ],
+            },
+          ],
+        },
+        //galley
+        {
+          gallery: [
+            {
+              galleryTitle: "Our Gallery",
+              imgPreview: [
+                {
+                  galleryImg: "img-gallery-1",
+                  galleryImg: "img-gallery-2",
+                  galleryImg: "img-gallery-3",
+                  galleryImg: "img-gallery-4",
+                  galleryImg: "img-gallery-5",
+                  galleryImg: "img-gallery-6",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+
+      footerBottom: [
+        {
+          copiright: "Copiright (copy) gogrin all rights reserved.",
+          service: "Service",
+          prices: "Pricing Table",
+          contact: "Contact",
+        },
+      ],
     };
   },
 
