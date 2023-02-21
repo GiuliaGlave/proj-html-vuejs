a4ea5c8b0072e1f62903274d98fb4e539ad96ffe
GiuliaGlave <giulia.glave@gmail.com>
2023-02-21 13:09:51 +0100

    header data structure

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      headerTop: [
+        {
+          location: "&hearts; luogo",
+          email: "email@mail.com",
+          businessHours: "8AM- 8PM",
+          socialAccounts: [
+            {
+              facebookAccount: "fb-logo",
+              twitterAccount: "tw-logo",
+              instagramAccount: "ig-logo",
+              pinterestAccount: "pts-logo",
+            },
+          ],
+        },
+      ],
+
+      headerBottom: [
+        {
+          logo: "img-logo",
+          navbar: [
+            {
+              home: "link-home",
+              about: "link-about",
+              service: "link-service",
+              portfolio: "link-portfolio",
+              shop: "link-shop",
+              blog: "link-blog",
+              contact: "link-contact",
+            },
+          ],
+          search: "search-icon",
+          chart: "chart-icon",
+          button: "ORDER NOW",
+        },
+      ],
+    };
   },
 
   methods: {},
